ebc0271c46867e441daba63deea15391044f2cd5
AndriiKoshmanTakeoff <andrii.koshman@takeoff.com>
2021-10-01 02:12:24 +0300

    github actions .github/workflows/cd.yml

--- gitactions.sh
+++ gitactions.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-gcloud builds submit --tag=gcr.io/$GOOGLE_CLOUD_PROJECT/go-pets/go-pets-image .  --project $GOOGLE_CLOUD_PROJECT
+export OOGLE_APPLICATION_CREDENTIALS = ${{ secrets.GOOGLE_APPLICATION_CREDENTIALS }}
+gcloud builds submit --tag=gcr.io/${{ secrets.GOOGLE_CLOUD_PROJECT }}/go-pets/go-pets-image .  --project ${{ secrets.GOOGLE_CLOUD_PROJECT }}
 
-(cd terraform && terraform init && terraform apply -auto-approve)
+(cd terraform &&
+terraform init  -backend-config="bucket=roi-takeoff-user17-terraform-state" &&
+terraform apply -auto-approve)
