de3d537e6ae59dae443b130914a39156c178b83d
AndriiKoshmanTakeoff <andrii.koshman@takeoff.com>
2021-10-01 04:19:39 +0300

    git actions

--- gitactions.sh
+++ gitactions.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 #gcloud builds submit --tag=gcr.io/$GOOGLE_CLOUD_PROJECT/go-pets/go-pets-image .  --project $GOOGLE_CLOUD_PROJECT
+bucket="roi-takeoff-user17-terraform-state"
 (cd terraform &&
-terraform init -reconfigure <<< roi-takeoff-user17-terraform-state &&
+terraform init -reconfigure <<< $bucket &&
 terraform apply -auto-approve)
